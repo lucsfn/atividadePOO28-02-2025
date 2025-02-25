54a757491d83bf2e2a2a30636eaf767dd46cfc02
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-02-25 16:31:43 -0300

    logica de validar cpf

--- util/ValidaDados.cs
+++ util/ValidaDados.cs
@@ -1,13 +1,32 @@
-using System;
-
 namespace Util
 {
     public static class ValidaDados
     {
-        public static bool ValidaCPF(string cpf)
+        private static bool ValidaCPF(string cpf)
         {
-            if (cpf.Length != 11) return false;
-            else
+            // Essa função espera receber o CPF já num formato de 11 dígitos, ou seja, essa validação tem que ser feita na entrada dos daods
+
+            if (cpf.Distinct().Count() == 1) return false; // CPF não pode ter todos os números iguais
+
+            int[] pesos1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+            int[] pesos2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+            // CPF é validado a partir da verificação dos DVs, se o cálculo do DV com os 9 primeiros dígitos for igual ao DV informado inicialmente, o CPF tá validado
+
+            int primeiroDV = CalcularDigitoVerificador(cpf[..9], pesos1);
+            int segundoDV = CalcularDigitoVerificador(cpf[..9] + primeiroDV, pesos2);
+
+            return cpf.EndsWith(primeiroDV.ToString() + segundoDV.ToString());
+
         }
+
+        private static int CalcularDigitoVerificador(string numeros, int[] pesos)
+        {
+            int soma = numeros.Select((t, i) => (t - '0') * pesos[i]).Sum(); // Transforma cada valor da string em um inteiro, multiplica pelo peso e soma-os
+            int resto = soma % 11;
+            return resto < 2 ? 0 : 11 - resto;
+        }
+
+
     }
 }
